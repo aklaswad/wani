7a052a36329a9a69d064d17374cf37bb8e0da80d
Akira Sawada <akira@peatix.com>
2015-03-22 18:01:58 +0900

    Clean up and OOPed site.js

--- sample/site.js
+++ sample/site.js
@@ -1,47 +1,91 @@
 $(function () {
-  // Synth
-  var ctx = Waml.getAudioContext();
-  var tri = Waml.createModule('TriOscillator');
+  function App () {
+    return this.init();
+  }
 
-  // Effect
-  var trm = Waml.createModule('SimpleTremolo');
-  trm.depth.value = 2.0;
-  trm.frequency.value = 0.0;
-  tri.connect(trm.inlet);
+  App.prototype.init = function () {
+    // DSP
+    var ctx = this.ctx = Waml.getAudioContext();
+    this.masterOut = ctx.createGain();
+    this.masterOut.gain.value = 0.3;
+    this.effects = [];
+    this.effectInstances = [];
+    this.primarySynth = Waml.createModule('TriOscillator');
+    this.renderer = Waml.Web.createWaveFormRenderer('waveform', {
+      waveColor: '#f84',
+      backgroundColor: '#fff',
+      centerLine: '#abc'
+    });
 
-  var mod = ctx.createOscillator();
-  mod.frequency.value = 0.1;
-  mod.start();
-  var modrange = ctx.createWaveShaper();
-  modrange.curve = new Float32Array([0.4,5]);
-  mod.connect(modrange);
-  modrange.connect(trm.frequency);
-  trm.connect(ctx.destination);
+    // DSP chain
+    this.primarySynth.connect(this.masterOut);
+    this.masterOut.connect(this.renderer);
+    this.masterOut.connect(ctx.destination);
+    // UIs
+    Waml.onmoduleload = function (module) {
+      Waml.describe(module.name, function (message) {
+        var $console = $('#js-console');
+        var html = $console.html();
+        $console.html( html + "<br />" + message );
+      });
+    };
 
-  var kb = document.getElementById('keyboard');
-  kb.addEventListener('change', function(e) {
-    var note = e.note;
-    if ( note[0] ) {
-      tri.noteOn(note[1]);
-    }
-    else {
-      tri.noteOff();
-    }
-  });
+    this.updateUI();
+    this.initKeyboard();
+  };
 
-  var renderer = Waml.Web.createWaveFormRenderer('waveform', {
-    waveColor: '#f84',
-    backgroundColor: '#fff',
-    centerLine: '#abc'
-  });
-  trm.connect(renderer);
-  Waml.onmoduleload = function (module) {
-    Waml.describe(module.name, function (message) {
-  console.log(message);
-      var $console = $('#js-console');
-      var html = $console.html();
-      $console.html( html + "<br />" + message );
+  App.prototype.updateUI = function () {
+    var synthesizers = Waml.listSynthesizers();
+    var effects = Waml.listEffects();
+    var i,len;
+    var $synthlist = $('.js-list-synthesizers').empty();
+    $.each(synthesizers, function (idx,name) {
+      $synthlist.append( $('<option>').text(name) );
+    });
+    var $effectlist = $('.js-list-effects').empty();
+    $.each(effects, function (idx,name) {
+      $effectlist.append( $('<option>').text(name) );
     });
   };
-  Waml.Web.loadScriptFromURL('SimpleAutoWah.js');
+
+  App.prototype.makeDSPChain = function() {
+    this.primarySynth.disconnect();
+    $.each( this.effectInstances, function( idx, effect ) {
+      effect.disconnect();
+    });
+    this.effectInstances = [];
+    var module, last = this.primarySynth;
+    var record = '';
+    $.each( this.effects, function(idx,name) {
+      module = Waml.createModule(name);
+      last.connect( module );
+      record += last.name + '.connect(' + module.name + ")\n";
+      last = module;
+    });
+    last.connect(this.masterOut);
+    record += last.name + ".connect(masterOut)\n";
+    $('#js-circuit').text(record);
+  }
+
+  App.prototype.loadModule = function(name) {
+    this.effects.push(Waml.createModule(name));
+    this.updateUI();
+    makeDSPChain();
+  };
+
+  App.prototype.initKeyboard = function () {
+    var kb = this.kb = document.getElementById('keyboard');
+    var that = this;
+    kb.addEventListener('change', function(e) {
+      var note = e.note;
+      if ( note[0] ) {
+        that.primarySynth.noteOn(note[1]);
+      }
+      else {
+        that.primarySynth.noteOff();
+      }
+    });
+  };
+
+  var app = new App();
 });
