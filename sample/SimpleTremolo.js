a6bf7ac3d9662b57be31d34958db69533107def2
Akira Sawada <akira@peatix.com>
2015-03-29 01:45:11 +0900

    Replace DSP circuit w/ auto generated by http://aklaswad.github.io/alligata/builder.html

--- sample/SimpleTremolo.js
+++ sample/SimpleTremolo.js
@@ -3,41 +3,61 @@
 
   function SimpleTremolo(ctx) {
     this.ctx = ctx;
+    var lfo = ctx.createOscillator();
+    lfo.frequency.value = 0;
+    var that = this;
     var inlet = this.inlet = ctx.createGain();
     var outlet = this.outlet = ctx.createGain();
+    outlet.gain.value = 0;
+
+/*
+audioParam('speed',5);
+audioParam('depth',0.6);
+
+lfo.frequency = :speed;
+outlet = inlet;
+outlet.gain = lfo * 0.5 * :depth + (1 - :depth )
+
+*/
+
+var io = (function (ctx) {
+  var io = {};
+  var nodes = [];
+  nodes[1] = io['speed'] = Wani.createAudioParam(ctx,5.0);
+  nodes[2] = io['depth'] = Wani.createAudioParam(ctx,0.6);
+  nodes[3] = lfo.frequency; // Existing node
+  nodes[4] = outlet; // Existing node
+  nodes[5] = inlet; // Existing node
+  nodes[6] = outlet.gain; // Existing node
+  nodes[7] = ctx.createGain();  // multi(7)
+  nodes[8] = nodes[7].gain;
+  nodes[8].value = 0.0;
+  nodes[9] = lfo; // Existing node
+  nodes[10] = ctx.createGain();  // multi(10)
+  nodes[11] = nodes[10].gain;
+  nodes[11].value = 0.0;
+  nodes[12] = Wani.createDCOffset(ctx,0.5); // number
+  nodes[13] = ctx.createGain();
+  nodes[13].gain.value = -1
+  nodes[14] = Wani.createDCOffset(ctx,1.0); // number
+
+  nodes[1].connect(nodes[3]);
+  nodes[5].connect(nodes[4]);
+  nodes[9].connect(nodes[7]);
+  nodes[12].connect(nodes[10]);
+  nodes[2].connect(nodes[11]);
+  nodes[10].connect(nodes[8]); // to output from multi(10)
+  nodes[7].connect(nodes[6]); // to output from multi(7)
+  nodes[13].connect(nodes[6]); // negativer for minus()
+  nodes[14].connect(nodes[6]);
+  nodes[2].connect(nodes[13]);// done
+return io;
+})(ctx);
 
 
-    var half = ctx.createGain();
-    half.gain.value = 0.5;
-    var osc = ctx.createOscillator();
-    osc.frequency.value = 5;
-    osc.connect(half);
-    var oscGain = ctx.createGain();
-    oscGain.gain.value = 0.0;
-    half.connect(oscGain);
-
-    var offset = Wani.createDCOffset(0);
-    var offsetBase = Wani.createDCOffset(1);
-    var offsetHalf = ctx.createGain();
-    offsetHalf.gain.value = 0.5;
-    offset.connect(offsetHalf);
-    offset.connect(oscGain.gain);
-    var neg = ctx.createGain();
-    neg.gain.value = -1;
-    offsetHalf.connect(neg);
-    this.neg = neg;
-    this.offset = offset;
-    outlet.gain.value = 1.0;
-    var shift = ctx.createGain();
-    offsetBase.connect(shift);
-    neg.connect(shift);
-    oscGain.connect(shift.gain);
-    inlet.connect(outlet);
-    shift.connect(outlet.gain);
-
-    this.frequency = osc.frequency;
-    this.depth = offset.gain;
-    osc.start();
+    this.speed = io.speed;
+    this.depth = io.depth;
+    lfo.start();
     return this;
   }
   SimpleTremolo.prototype = Object.create(Wani.Module.prototype);
@@ -49,13 +69,13 @@
     type: 'effect',
     create: SimpleTremolo,
     audioParams: {
-      frequency: {
+      speed: {
         description: 'frequency (hz)',
-        range: [0, 20],
+        range: [0, 20]
       },
       depth: {
         description: 'depth',
-        range: [0, 10],
+        range: [0, 3]
       },
     },
     // Specify the inlet as string. "inlet" should be default.
